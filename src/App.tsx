accfa3aa509ecb98ef6bf48b67c13e0bf753346b
TopMyster <topmyster@outlook.com>
2026-07-12 18:18:23 -0500

    feat: Turns FAQ list into an array and working search

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
+import { useState } from 'react'
 import Question from './Question.tsx'
 
 export default function App() {
+  const [searchQuery, setSearchQuery] = useState("")
+  const [currentSearch, setCurrentSearch] = useState("")
+  const FAQ = [
+    {
+      question: "hi",
+      answer: "hola"
+    },
+    {
+      question: "man",
+      answer: "boy"
+    }
+  ]
+
+  const filteredFAQ = FAQ.filter(faq =>
+    faq.question.toLowerCase().includes(searchQuery === "" ? "" :currentSearch.toLowerCase())
+  )
+
   return (
     <>
      <div className="content">
       <div className='header'>
-        <img src="https://assets.hackclub.com/flag-standalone.svg" className='hackclub'/>
+        <a href="https://hackclub.com/" target='_blank'><img src="https://assets.hackclub.com/flag-standalone.svg" className='hackclub'/></a>
         <h1>Macondo</h1> 
         <h2>FAQ</h2>
       </div>
-      
       <div className="search">
-        <input placeholder="Search your issue..."/>
-        <button>Search</button>
+        <input value={searchQuery} onChange={(e) => {setSearchQuery(e.target.value)}} placeholder="Search your issue..."/>
+        <button onClick={() => setCurrentSearch(searchQuery)}>Search</button>
       </div>
       <hr className="line"/>
       <div className="faq">
-        <Question 
-          question="I'm a complete beginner. Can I still join?" 
-          answer= "Absolutely! We have beginner-friendly guided projects and a community where you can ask for help anytime."
-        />
-      </div>
+        {filteredFAQ.map(faq => 
+          <Question question={faq.question} answer={faq.answer}/>
+        )}
+      </div>    
+      <footer>
+        <i>This website is not affiliated with Hack Club</i>
+      </footer>
      </div>
     </>
   )
